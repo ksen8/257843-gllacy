a33141f74abb153869ce27d49488fc41dd52d99d
ksen8 <ksenia_591@mail.ru>
2016-11-22 00:20:44 +0300

    Изменила чекбокс, js для формы

---
+++ js/popup.js
@@ -0,0 +1,26 @@
+    var link = document.querySelector(".js-open-modal");
+	var popup = document.querySelector(".modal-feedback");
+	var close = popup.querySelector(".modal-feedback-close");
+    var overlay = document.querySelector(".modal-overlay");
+
+	link.addEventListener("click", function(event) {
+      event.preventDefault();
+	  overlay.classList.add("modal-overlay-show");
+	  popup.classList.add("modal-feedback-show");
+	});
+	
+    close.addEventListener("click", function(event) {
+      event.preventDefault();
+      popup.classList.remove("modal-feedback-show");
+	  overlay.classList.remove("modal-overlay-show");
+    });
+	
+	window.addEventListener("keydown", function(event) {
+      if (event.keyCode === 27) {
+        if (popup.classList.contains("modal-feedback-show")) {
+            popup.classList.remove("modal-feedback-show");
+        }
+      }
+    });
+
+
